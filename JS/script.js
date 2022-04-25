e97b3d92c7c201e677ff7ab017cbd10f587c2cf6
Nathaniel Tang <nathanieltang22@gmail.com>
2022-04-25 14:49:09 -0400

    Fixed so that previous display is cleared and items are no longer doubled up

--- JS/script.js
+++ JS/script.js
@@ -1,49 +1,65 @@
 let myLibrary = [];
 
-
-function book (title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        console.log(this.title + "by" + this.author + "is" + this.pages + "long.")
-    }
+function book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    console.log(this.title + "by" + this.author + "is" + this.pages + "long.");
+  };
 }
 
-const shelf = document.querySelector('.shelf')
+const shelf = document.querySelector(".shelf");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
 
 function addBookToLibrary() {
-    let bookTitle = document.getElementById('title').value;
-    let bookAuthor = document.getElementById('author').value;
-    let bookPages = document.getElementById('pages').value;
+  let bookTitle = title.value;
+  let bookAuthor = author.value;
+  let bookPages = pages.value;
 
-    let newBook = new book(bookTitle, bookAuthor, bookPages, true)
+  let newBook = new book(bookTitle, bookAuthor, bookPages, true);
 
-    myLibrary.push(newBook)
-    console.log(myLibrary)
+  myLibrary.push(newBook);
+  console.log(myLibrary);
 }
 
-const addToLibrary = document.getElementById('addToLibrary');
-addToLibrary.addEventListener('click', addBookToLibrary)
-addToLibrary.addEventListener('click', createLibrary)
+const addToLibrary = document.getElementById("addToLibrary");
+addToLibrary.addEventListener("click", addBookToLibrary);
+addToLibrary.addEventListener("click", createLibrary);
+addToLibrary.addEventListener("click", clearForm);
 
-
-// test
+/* test
 
 const catch22 = new book("Catch 22", "Joseph Heller", 522, true)
 myLibrary.push(catch22)
 
 const catch21 = new book("Catch 21", "Joseph Heller", 521, false)
-myLibrary.push(catch21)
+myLibrary.push(catch21) */
+
 
 
 function createLibrary() {
+    shelf.innerHTML = '';
     myLibrary.forEach((element, index) => {
-    let bookSpot = document.createElement('p');
-    let bookDetails = document.createTextNode(`title: ${myLibrary[index].title}
-    author: ${myLibrary[index].author} pages: ${myLibrary[index].pages}`)
-    bookSpot.appendChild(bookDetails);
-    shelf.appendChild(bookSpot)
-})
+        let bookSpot = document.createElement("div");
+        bookSpot.classList.add("card");
+        let bookDetails = document.createElement("p");
+        bookSpot.appendChild(bookDetails)
+        bookDetails.textContent = (`title: ${myLibrary[index].title}
+        author: ${myLibrary[index].author} pages: ${myLibrary[index].pages}`)
+        shelf.appendChild(bookSpot)
+    })
 }
+
+function clearForm() {
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+}
+
+
+//next, add function to clear values of input when submitting. Also
+//clear the library when clicking button so it doesn't overlap
